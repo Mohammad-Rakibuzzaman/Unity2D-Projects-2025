3e4b914cf6512fecd146bf6707e3039a70bff573
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-04-08 11:18:18 +0600

    serializer understand

--- Medikit Driver/Assets/Driver.cs
+++ Medikit Driver/Assets/Driver.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,15 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
+
+
+
+
+
+
+
+
